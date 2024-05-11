f8f73f31d23510235a48605725a5086a804f136b
eawadelk <eawadelk@jaguarlandrover.com>
2024-05-11 18:59:42 +0100

    communicating between two processes

--- main_A.cpp
+++ main_A.cpp
@@ -1,53 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <cstring>
+#include <iostream>
 #include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(1);
-}
 
-int main(int argc, char *argv[])
+/* create a socket file descriptor FD using socket()
+   then do
+        1 create a server address struct
+        2 bind the socket fd with the address structure
+        3 listen using the socket fd
+        4 accept connection requests
+        5 reveive until exit is called
+   then*/
+int main()
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd,
-                 (struct sockaddr *) &cli_addr,
-                 &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0;
+	// creating socket
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// specifying the address
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(8080);
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+	// binding socket.
+	bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+	// listening to the assigned socket
+	listen(serverSocket, 5);
+	// accepting connection request
+	int clientSocket{accept(serverSocket, nullptr, nullptr)};
+
+	// recieving data
+	char buffer[1024] = { 0 };
+    std::string std_buffer;
+    while(std::strcmp("exit",std_buffer.c_str()) || std_buffer.length()==0)
+    {
+        std::cout<<"------------------------"<<std::endl;
+        bzero(buffer,1024);
+        auto ret = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if((0 ==ret) || (-1 == ret))
+            break;
+        std_buffer = std::string(buffer);
+        std::cout <<"received ...>> "<< buffer << std::endl;
+    }
+	close(serverSocket);
+
+	return 0;
 }
--- main_B.cpp
+++ main_B.cpp
@@ -1,57 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+// C++ program to illustrate the client application in the
+// socket programming
+#include <cstring>
+#include <iostream>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
 
-void error(const char *msg)
+int main()
 {
-    perror(msg);
-    exit(0);
-}
+	// creating socket
+	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
+	// specifying address
+	sockaddr_in serverAddress;
+    const char * temp {"localhost"};
+    char hostname[128];
+    gethostname(hostname, sizeof(hostname));
+    struct hostent * ss = gethostbyname(hostname);
+    if (ss == NULL)
+    {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0)
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0)
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
+	serverAddress.sin_family = AF_INET;
+    // bcopy((char *)ss->h_addr,(char *)&serverAddress.sin_addr.s_addr, ss->h_length);
+	serverAddress.sin_port = htons(8080);
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+	// sending connection request
+	connect(clientSocket, (struct sockaddr*)&serverAddress,
+			sizeof(serverAddress));
+
+	// sending data
+    char check{'a'};
+    std::string usr_msg{""};
+    while("exit"!=usr_msg)
+    {
+        std::cin>>usr_msg;
+        send(clientSocket,usr_msg.c_str() , usr_msg.length(), 0);
+    }
+
+
+	// closing socket
+	close(clientSocket);
+
+	return 0;
 }
